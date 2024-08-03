13eb70ebea10cd516b540f99ebba3553f08f6747
combikms <combikms@naver.com>
2024-08-03 11:34:32 +0900

    :tada: CRUD 테스트용 서버 만들기

---
+++ server.js
@@ -0,0 +1,86 @@
+const express = require('express')
+const cors = require('cors');
+const app = express()
+
+const post = [
+    {
+        title: '학교 근로하면 달에 얼마받아요?',
+        content: '평균',
+        id: 0
+    },
+    {
+        title: '침착맨 전문 시청 1팀 카드지갑 잃어버리신 분?',
+        content: '쪽지주세용 (조공 있으면 더 좋고 ㅎ 농담임)',
+        id: 1
+    },
+    {
+        title: '수강꾸러미',
+        content: '확정 뜨는게 언제였지? 왜 확정 전이라 뜨냐',
+        id: 2
+    },
+    {
+        title: '반시공님들',
+        content: '상담내용에머씀?난 그냥 교육과정대로 잘했는지 적었는데.\n' +
+            '그리고 이번주 화욜에 제출했는데 아직까지 미승인인데 다들 승인남?지도교수ㅇㅇㅇ 임',
+        id: 3
+    }
+]
+
+app.use(cors());
+app.use(express.json())
+app.use(express.urlencoded({ extended: true }))
+
+app.listen(8080, () => {
+    console.log('http://localhost:8080 에서 서버 실행중')
+})
+
+app.get('/', (req, res) => {
+    res.send(post)
+})
+
+app.get('/post/:id', (req, res) => {
+    const req_post = post.find(item => item.id == req.params.id)
+    console.log(`${req.params.id}번 게시글을 전송합니다.`)
+    console.log(req_post)
+    res.send(req_post)
+})
+
+app.post('/', (req, res) => {
+    const data = req.body
+    if (post.length == 0) {
+        data.id = 0
+    } else {
+        data.id = post[post.length - 1].id + 1
+    }
+    post.push(req.body)
+    console.log(post)
+})
+
+app.delete('/:id', (req, res) => {
+    const postId = parseInt(req.params.id, 10);
+    console.log(postId + '번 게시글 삭제 요청');
+
+    // 게시글을 찾아서 삭제
+    const postIndex = post.findIndex(post => post.id === postId);
+    if (postIndex !== -1) {
+        post.splice(postIndex, 1);
+        res.status(200).send({ message: 'Post deleted successfully' });
+    } else {
+        res.status(404).send({ message: 'Post not found' });
+    }
+});
+
+
+app.put('/:id', (req, res) => {
+    const postId = parseInt(req.params.id, 10);
+    const updatedData = req.body;
+    console.log(postId + '번 게시글 수정 요청', updatedData);
+
+    const postIndex = post.findIndex(post => post.id === postId);
+    if (postIndex !== -1) {
+        post[postIndex] = { ...post[postIndex], ...updatedData };
+        res.status(200).send({ message: 'Post updated successfully', post: [postIndex] });
+    } else {
+        res.status(404).send({ message: 'Post not found' });
+    }
+});
